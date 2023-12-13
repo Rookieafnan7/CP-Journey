abeb96b1c79d94901e3671ddd74280860d5f4a85
Smortr Developer <137102622+smortr-dev@users.noreply.github.com>
2023-12-13 19:58:47 +0530

    Day 8

---
+++ minimum-window-substring-important-hard.cpp
@@ -0,0 +1,87 @@
+// Given two strings s and t of lengths m and n respectively, return the minimum window
+// substring
+//  of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+// The testcases will be generated such that the answer is unique.
+
+// Example 1:
+
+// Input: s = "ADOBECODEBANC", t = "ABC"
+// Output: "BANC"
+// Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+// Example 2:
+
+// Input: s = "a", t = "a"
+// Output: "a"
+// Explanation: The entire string s is the minimum window.
+// Example 3:
+
+// Input: s = "a", t = "aa"
+// Output: ""
+// Explanation: Both 'a's from t must be included in the window.
+// Since the largest window of s only has one 'a', return empty string.
+
+// Constraints:
+
+// m == s.length
+// n == t.length
+// 1 <= m, n <= 105
+// s and t consist of uppercase and lowercase English letters.
+
+// Thanks zjh08177 for this great idea. Let me explain this algorithm.
+
+// 1. Use two pointers: start and end to represent a window.
+// 2. Move end to find a valid window.
+// 3. When a valid window is found, move start to find a smaller window.
+// To check if a window is valid, we use a map to store (char, count) for chars in t.
+//  And use counter for the number of chars of t to be found in s.
+//  The key part is m[s[end]]--;. We decrease count for each char in s.
+// If it does not exist in t, the count will be negative.
+
+// To really understand this algorithm, please see my code which is much clearer,
+//  because there is no code like if(map[s[end++]]++>0) counter++;.
+
+#include <cstddef>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+string minWindow(string s, string t)
+{
+    unordered_map<char, int> m;
+    // Statistic for count of char in t
+    for (auto c : t)
+        m[c]++;
+    // counter represents the number of chars of t to be found in s.
+    size_t start = 0, end = 0, counter = t.size(), minStart = 0, minLen = INT_MAX;
+    size_t size = s.size();
+
+    // Move end to find a valid window.
+    while (end < size)
+    {
+        // If char in s exists in t, decrease counter
+        if (m[s[end]] > 0)
+            counter--;
+        // Decrease m[s[end]]. If char does not exist in t, m[s[end]] will be negative.
+        m[s[end]]--;
+        end++;
+        // When we found a valid window, move start to find smaller window.
+        while (counter == 0)
+        {
+            if (end - start < minLen)
+            {
+                minStart = start;
+                minLen = end - start;
+            }
+            m[s[start]]++;
+            // When char exists in t, increase counter.
+            if (m[s[start]] > 0)
+                counter++;
+            start++;
+        }
+    }
+    if (minLen != INT_MAX)
+        return s.substr(minStart, minLen);
+    return "";
+}
